679796ace5b47d8d5edfaf1c139bfe0f67829b06
Mohd Zainul <mohdzainul939@gmail.com>
2025-06-11 08:28:31 +0530

    initial commit

---
+++ 206_Reverse_Linked_List.java
@@ -0,0 +1,15 @@
+class Solution {
+    public Node reverseList(Node head) {
+        Node prev = null;
+        Node curr = head;
+
+        while (curr != null) {
+            Node nextNode = curr.next; 
+            curr.next = prev;              
+            prev = curr;                   
+            curr = nextNode;               
+        }
+
+        return prev; 
+    }
+}
